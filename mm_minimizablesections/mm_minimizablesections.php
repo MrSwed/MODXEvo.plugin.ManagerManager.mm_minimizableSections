c179aeff1f3c3319d3a953d4e4555bc5ee5874ec
MrSwed <webmaster@sdcollection.com>
2015-05-30 16:25:42 +0300

    Refactoring

--- mm_minimizablesections/mm_minimizablesections.php
+++ mm_minimizablesections/mm_minimizablesections.php
@@ -3,11 +3,11 @@
  * mm_minimizablesections
  * @version 0.2 (2015-05-30)
  *
- * @desc A widget for ManagerManager plugin that allows one or a few sections to be minimizable on the document edit page.
+ * @desc A widget for ManagerManager plugin that allows one, few or all sections to be minimizable on the document edit page.
  *
  * @uses ManagerManager plugin 0.6.2.
  *
- * @param $sections {comma separated string} - The id(s) of the sections this should apply to. @required
+ * @param $sections {comma separated string} - The id(s) of the sections this should apply to. Use '*' for apply to all. @required
  * @param $roles {comma separated string} - The roles that the widget is applied to (when this parameter is empty then widget is applied to the all roles). Default: ''.
  * @param $templates {comma separated string} - Id of the templates to which this widget is applied (when this parameter is empty then widget is applied to the all templates). Default: ''.
  * @param $minimized {comma separated string} - The id(s) of the sections this should be minimized by default.
@@ -49,15 +49,15 @@ function mm_minimizablesections($sections, $roles = '', $templates = '',$minimiz
   $sections = makeArray($sections);
   $minimized = makeArray($minimized);
 
-  $output .= "//---------- mm_minimizablesections :: Begin -----\n";
-  $sectionIDS = array_map("prepareSection",$sections);
-  $sectionIDSmin = array_map("prepareSection",$minimized);
-  $output .= '$j("'.implode(",",$sectionIDS).'","#documentPane").addClass("minimizable").on("click",function(){
+  $sections = array_map("prepareSection",$sections);
+  $minimized = array_map("prepareSection",$minimized);
 
+  $output .= "//---------- mm_minimizablesections :: Begin -----\n";
+  $output .= '$j("'.implode(",",$sections).'","#documentPane").addClass("minimizable").on("click",function(){
      var _t = $j(this);
      _t.next().slideToggle(400,function(){_t.toggleClass("minimized");})
     });
-    $j(".minimizable").filter("'.implode(",",$sectionIDSmin).'").addClass("minimized").next().hide();
+    $j(".minimizable").filter("'.implode(",",$minimized).'").addClass("minimized").next().hide();
   ';
 
   $output .= "//---------- mm_minimizablesections :: End -----\n";
