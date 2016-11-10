2219e6ca0350d1f35f4bd1cee7ab08847214f783
MrSwed <webmaster@sdcollection.com>
2016-11-10 15:39:48 +0300

    refactors

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# mm_minimizablesections
+# mm_minimizableSections
 A widget for ManagerManager plugin that allows one, few or all sections to be minimizable on the document edit page.
 Can significantly reduce the space occupied on the screen, which is useful in the case of using a large number of TV parameters, 
 especially when using multiparameters \(see. [mm_ddMultipleFields](http://code.divandesign.biz/modx/mm_ddmultiplefields/)\).
 
 ## Install
-Copy folder mm_minimizablesections to __/assets/plungins/managermanager/widgets/__.
+Copy folder mm_minimizableSections to __/assets/plungins/managermanager/widgets/__.
 
 ## Use
 Read this documentation of [ManagerManager](http://code.divandesign.biz/modx/managermanager).
@@ -21,13 +21,13 @@ To specify multiple values, each parameter can be set separated by commas.
 
 Apply to all sections for all roles and templates
 ```
-mm_minimizablesections('*');
+mm_minimizableSections('*');
 ```
 Apply to the content and Template Variables sections for users with role "1" editing the documents with the template ID equals "3"
 ```
-mm_minimizablesections('content,tvs', '1', '3');
+mm_minimizableSections('content,tvs', '1', '3');
 ```
 Apply to all sections and set minimized Template variables and photos sections by default
 ```
-mm_minimizablesections('*','','','tvs,photos');
+mm_minimizableSections('*','','','tvs,photos');
 ```
--- mm_minimizablesections/mm_minimizablesections.php
+++ mm_minimizablesections/mm_minimizablesections.php
@@ -1,69 +1,72 @@
 <?php
 /**
- * mm_minimizablesections
- * @version 0.2 (2015-05-30)
- *
- * @desc A widget for ManagerManager plugin that allows one, few or all sections to be minimizable on the document edit page.
- *
- * @uses ManagerManager plugin 0.6.2.
- *
- * @param $sections {comma separated string} - The id(s) of the sections this should apply to. Use '*' for apply to all. @required
- * @param $roles {comma separated string} - The roles that the widget is applied to (when this parameter is empty then widget is applied to the all roles). Default: ''.
- * @param $templates {comma separated string} - Id of the templates to which this widget is applied (when this parameter is empty then widget is applied to the all templates). Default: ''.
- * @param $minimized {comma separated string} - The id(s) of the sections this should be minimized by default.
- *
- * @author Sergey Davydov <webmaster@sdcollection.com>
- *
- * @copyright 2015
- */
+	* mm_minimizableSections
+	* @version 0.2 (2015-05-30)
+	*
+	* @desc A widget for ManagerManager plugin that allows one, few or all sections to be minimizable on the document edit page.
+	*
+	* @uses ManagerManager plugin 0.6.2.
+	*
+	* @param $sections {comma separated string} - The id(s) of the sections this should apply to. Use '*' for apply to all. @required
+	* @param $roles {comma separated string} - The roles that the widget is applied to (when this parameter is empty then widget is applied to the all roles). Default: ''.
+	* @param $templates {comma separated string} - Id of the templates to which this widget is applied (when this parameter is empty then widget is applied to the all templates). Default: ''.
+	* @param $minimized {comma separated string} - The id(s) of the sections this should be minimized by default.
+	*
+	* @author Sergey Davydov <webmaster@sdcollection.com>
+	*
+	* @copyright 2015
+	*/
 
 function prepareSection($section) {
- switch ($section) {
-  case 'access':
-   return "#sectionAccessHeader";
-   break;
-  case '*':
-   return ".sectionHeader";
-   break;
-  default:
-   $section = prepareSectionId($section);
-   return "#{$section}_header";
-   break;
- }
+	switch ($section) {
+		case 'access':
+			return "#sectionAccessHeader";
+			break;
+		case '*':
+			return ".sectionHeader";
+			break;
+		default:
+			$section = prepareSectionId($section);
+
+			return "#{$section}_header";
+			break;
+	}
 }
 
-function mm_minimizablesections($sections, $roles = '', $templates = '',$minimized = '') {
- if (!useThisRule($roles, $templates)){return;}
- 
- global $modx;
- $e = &$modx->Event;
- $site = $modx->config['site_url'];
- $widgetDir = $site.'assets/plugins/managermanager/widgets/mm_minimizablesections/';
+function mm_minimizableSections($sections, $roles = '', $templates = '', $minimized = '') {
+	if (!useThisRule($roles, $templates)) {
+		return;
+	}
 
- $output='';
- if ($e->name == 'OnDocFormPrerender') {
-  $output .= includeJsCss($widgetDir.'minimizablesections.css', 'html');
+	global $modx;
+	$e = &$modx->Event;
+	$site = $modx->config['site_url'];
+	$widgetDir = $site.'assets/plugins/managermanager/widgets/mm_minimizablesections/';
 
-  $e->output($output);
- } else if ($e->name == 'OnDocFormRender') {
-  $sections = makeArray($sections);
-  $minimized = makeArray($minimized);
+	$output = '';
+	if ($e->name == 'OnDocFormPrerender') {
+		$output .= includeJsCss($widgetDir.'minimizablesections.css', 'html');
 
-  $sections = array_map("prepareSection",$sections);
-  $minimized = array_map("prepareSection",$minimized);
+		$e->output($output);
+	} else if ($e->name == 'OnDocFormRender') {
+		$sections = makeArray($sections);
+		$minimized = makeArray($minimized);
 
-  $output .= "//---------- mm_minimizablesections :: Begin -----\n";
-  $output .= '$j("'.implode(",",$sections).'","#documentPane").addClass("minimizable").on("click",function(){
+		$sections = array_map("prepareSection", $sections);
+		$minimized = array_map("prepareSection", $minimized);
+
+		$output .= "//---------- mm_minimizableSections :: Begin -----\n";
+		$output .= '$j("'.implode(",", $sections).'","#documentPane").addClass("minimizable").on("click",function(){
      var _t = $j(this);
      _t.next().slideToggle(400,function(){_t.toggleClass("minimized");})
     });
-    $j(".minimizable").filter("'.implode(",",$minimized).'").addClass("minimized").next().hide();
+    $j(".minimizable").filter("'.implode(",", $minimized).'").addClass("minimized").next().hide();
   ';
 
-  $output .= "//---------- mm_minimizablesections :: End -----\n";
+		$output .= "//---------- mm_minimizableSections :: End -----\n";
 
-  $e->output($output);
- }
+		$e->output($output);
+	}
 }
 
 ?>
